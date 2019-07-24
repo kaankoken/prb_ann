6ae2021bd45e6d023d694ade0d04de5e0e07c104
kaan.koken <kaan_taha_koken@hotmail.com>
2019-07-25 00:05:00 +0300

    Proje Created

---
+++ prb_map_ann_v2.py
@@ -0,0 +1,17 @@
+#%%
+import pandas as pd
+import numpy as np
+#from google.colab import drive, files
+import matplotlib.pylot as pylot
+
+#%% 
+class PreProcess:
+#%%
+class GoogleDrive:
+    def mount_drive(self):
+        drive.mount('/content/gdrive')
+        return
+    
+#%%
+
+class Model:
