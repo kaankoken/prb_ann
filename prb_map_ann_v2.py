badddb2275a8f5729147a723c18000c7701d0c19
kaan.koken <kaan_taha_koken@hotmail.com>
2019-07-25 01:06:24 +0300

    preprocess class created

--- prb_map_ann_v2.py
+++ prb_map_ann_v2.py
@@ -3,16 +3,27 @@ import os
 import pandas as pd
 import numpy as np
 from google.colab import drive, files
-import matplotlib.pylot as pylot
+import matplotlib.pyplot as pylot
 
-#%% 
+#%%
 class PreProcess:
+    __csv_path: str
+    def __init__(self, csv_path: str = 'Colab Notebooks/bq-results-20190628-104852-awu4v6ig6fyq.csv'):
+        if csv_path.startswith('/'):
+            self.__csv_path = '.' + csv_path
+        else:
+            self.__csv_path = './' + csv_path
+
+    def read_file(self):
+        df = pd.read_csv(self.__csv_path)
+        return df
+
 #%%
 class GoogleDrive:
     #mounts the drive and change the file path to google drive's path
     def mount_drive(self):
         drive.mount('/content/gdrive')
-        os.chdir('/gdrive')
+        os.chdir('./gdrive/My Drive')
         return
     
     #creates file at the given path
@@ -29,5 +40,5 @@ class GoogleDrive:
     def upload_files(self):
         uploaded = files.upload()
         return
-
-class Model:
+#%%
+    class Model:
