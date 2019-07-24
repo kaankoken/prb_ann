cd99b48c4d0ee190709d2f0b591ec3a62697967c
kaan.koken <kaan_taha_koken@hotmail.com>
2019-07-25 00:30:16 +0300

    drive class created

--- prb_map_ann_v2.py
+++ prb_map_ann_v2.py
@@ -1,17 +1,33 @@
 #%%
+import os
 import pandas as pd
 import numpy as np
-#from google.colab import drive, files
+from google.colab import drive, files
 import matplotlib.pylot as pylot
 
 #%% 
 class PreProcess:
 #%%
 class GoogleDrive:
+    #mounts the drive and change the file path to google drive's path
     def mount_drive(self):
         drive.mount('/content/gdrive')
+        os.chdir('/gdrive')
         return
     
-#%%
+    #creates file at the given path
+    def create_file(self, path: str):
+        os.mkdir(path)
+        return
+    
+    #deletes the file at the given path
+    def remove_file(self, path: str):
+        os.rmdir(path)
+        return
+
+    #it allows you to upload files to google drive
+    def upload_files(self):
+        uploaded = files.upload()
+        return
 
 class Model:
