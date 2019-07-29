2d58eafe24f99c4c8e9790176b6b155738bebc99
kaan.koken <kaan_taha_koken@hotmail.com>
2019-07-29 10:03:16 +0300

    sort_values is changed and 3 new methods added

--- prb_map_ann_v2.py
+++ prb_map_ann_v2.py
@@ -29,14 +29,52 @@ class PreProcess:
         converted = tm.timestamp() * 1000 
         return converted
     
-    def sort_values(self, data_frame: pd.DataFrame, value: str = 'user_id', asc: bool = False):
-        data = data_frame.sort_values([i for i in value], ascending=asc).reset_index(drop=True)
-        return data
+    def sort_values(self, data_frame: pd.DataFrame):
+        x = data_frame.groupby('user_id', sort=True)
+        t = []
+        [t.append(value) for key, value in x]
+        x = []
+        [x.append(i.sort_values('timestamp',ascending=True).reset_index(drop=True)) for i in t]
+        
+        return x
     
     def split_to_trip(self, data_frame: pd.DataFrame):
         data_frame['trip'] = (data_frame['timestamp'] - data_frame[i]['timestamp'].shift(1) > 60000).cumsum()
         return data_frame
 
+    def divide_to_trip(self, data: pd.DataFrame):
+        trip = data.index[np.where(data['trip'] - data['trip'].shift(1) > 0)]
+        tripCount = trip.values.size
+        data = data.drop('trip', axis=1)
+
+        changedData = []
+
+        endpoint = 0
+        if (tripCount > 0):
+            for i in range(0, tripCount):
+                if (i == 0):
+                    changedData.append(data.loc[0: trip[i] -1].reset_index(drop=True))
+                else:
+                    changedData.append(data.loc[trip[i - 1]: trip[i] -1].reset_index(drop=True))
+
+                endpoint = i
+            changedData.append(data.loc[trip[endpoint]: len(data)].reset_index(drop=True))
+        else:
+            changedData.append(data.loc[0: len(data)].reset_index(drop=True))
+
+        return changedData
+    
+    def flatten(self, data: pd.DataFrame):
+        x = [e for sl in data for e in sl]
+        return x
+    
+    def drop_if_small(self, data: pd.DataFrame, size: int):
+        y = []
+        for i in data:
+            if len(i) > size:
+                y.append(i)
+
+        return y
 #%%
 class GoogleDrive:
     #mounts the drive and change the file path to google drive's path
@@ -73,7 +111,16 @@ data_frame = pre_process.read_file(path)
 
 #%%
 data_frame['timestamp'] = data_frame['timestamp'].apply(pre_process.convert_timestamps)
-sort_parameter = ['user_id', 'timestamp']
-data_frame = pre_process.sort_values(data_frame, sort_parameter, False)
+data_frame = pre_process.sort_values(data_frame)
+data_frame = pre_process.drop_if_small(data_frame, 4)
 
 #%%
+for i in range(0,len(data_frame)):
+  data_frame[i]['trip'] = (data_frame[i]['timestamp'] - data_frame[i]['timestamp'].shift(1) > 60000).cumsum()
+#%%
+for i in range(0,len(data_frame)):
+  data_frame[i] = pre_process.divide_to_trip(data_frame[i])
+data_frame = pre_process.flatten(data_frame)
+
+#%%
+data_frame = pre_process.drop_if_small(data_frame, 4)
